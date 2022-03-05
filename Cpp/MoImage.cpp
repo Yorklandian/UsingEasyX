4c0282a9b5827410e11cfc1403a46ca6169e4f8c
YK <1627936869@qq.com>
2022-03-05 15:47:59 +0800

    将Move，Turn和SlideTo的实现迁移到MoImage类中

--- Cpp/MoImage.cpp
+++ Cpp/MoImage.cpp
@@ -15,3 +15,59 @@ MoImage::~MoImage()
 {
 	cout <<"Destructing MoImage"<<endl;
 }
+
+void MoImage::Move(int steps)
+{
+	double deltaX = sin(this->rotation);
+	double deltaY = cos(this->rotation);
+	for (int j = 0; j < steps; j++)
+	{
+		this->x -= deltaX * 10;
+		this->y -= deltaY * 10;
+		cleardevice();
+		putimage(this->x, this->y, this->img);
+		Sleep(200);
+	}
+}
+
+void MoImage::Turn(float radian)
+{
+	this->rotation -= radian;
+	if(this->rotation >= 2 * PI)
+	{
+		this->rotation -= 2 * PI;
+	}
+	rotateimage(img, img, -radian, BLACK, false, true);
+	cleardevice();
+	putimage(x, y, img);
+}
+
+void MoImage::SlideTo(int x, int y, double speed)
+{
+	int deltaX = x - this->x;
+	int deltaY = y - this->y;
+	deltaX *= speed;
+	deltaY *= speed;
+	if (x >= this->x)
+	{
+		while (x > this->x)
+		{
+			this->x += deltaX;
+			this->y += deltaY;
+			cleardevice();
+			putimage(this->x, this->y, this->img);
+			Sleep(30);
+		}
+	}
+	else
+	{
+		while (x < this->x)
+		{
+			this->x += deltaX;
+			this->y += deltaY;
+			cleardevice();
+			putimage(this->x, this->y, this->img);
+			Sleep(30);
+		}
+	}
+}
--- Cpp/MoImage.h
+++ Cpp/MoImage.h
@@ -2,6 +2,10 @@
 #include <graphics.h>
 #include <conio.h>
 #include <string>
+#ifndef PI
+#define PI 3.1415926535
+#endif
+
 using namespace std;
 
 //自定义的可移动图片类，包含x，y坐标，id，旋转弧度等字段。
@@ -17,5 +21,10 @@ public:
 	MoImage();
 	MoImage(int id, IMAGE* img, int x, int y);
 	~MoImage();
+
+	void Move(int steps);
+	void Turn(float radian);
+	void SlideTo(int x, int y, double speed);
+
 };
 
